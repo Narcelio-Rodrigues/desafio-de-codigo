b71b74cb7691c730215b72090cf488fe1e4b2b75
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-06-23 00:04:35 -0300

    dia-028

--- progresso/semana-04/dia-028/exercicios/script1.js
+++ progresso/semana-04/dia-028/exercicios/script1.js
@@ -1,5 +1,13 @@
 let num1 = []
 console.log(num1)
 
-let num2 = [5, 8, 2, 0]
+let num2 = [3, 1, 2]
+
+// + um valor
+num2 [2] = 4
+num2 [3] = 9
+num2.push(90)
+num2.length
+num2.sort()
+
 console.log(num2)
