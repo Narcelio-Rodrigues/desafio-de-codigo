9452aef767bd1dd2daed2ad95ed32c4ecceb1c16
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-07-05 23:29:43 -0300

    dia-041

---
+++ progresso/semana-06/dia-041/exercicios/script.js
@@ -0,0 +1,29 @@
+let ehLigado = false
+
+if (ehLigado){
+    console.log('Executou comando')
+}
+
+// 
+
+let possuiOvos = false
+let itensComprados = ''
+
+if(possuiOvos){
+    itensComprados = 'Leite'
+}
+
+console.log('item comprado: ' + itensComprados)
+
+// 
+
+let nivelDeFome = 1
+
+if(nivelDeFome === 1) {
+    console.log('Pouca fome')
+} else if (nivelDeFome === 2){
+    console.log('Muita fome')
+} else {
+    console.log('Você comeria mais que o pica pau')
+}
+
