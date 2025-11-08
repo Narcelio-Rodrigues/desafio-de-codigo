956a3607acca4e2d6ce41665919f90157c9af492
Narcélio Rodrigues <rodriguesnarcelio5@gmail.com>
2025-11-08 18:48:39 -0300

    Estruturas Condicionais

--- JavaScript/Exercício 1 - Calculadora de 4 operações/index.js
+++ JavaScript/Exercício 1 - Calculadora de 4 operações/index.js
@@ -1,3 +1,12 @@
+/*
+Calculadora de 4 Operações
+
+Escreve um programa em javascript que permita inserir dois valores numéricos e então calcule o resultado das quatro operações básicas (soma, subtração, multiplicação e divisão).
+
+Após calcular os resultados o programa deve exibi-los na tela.
+*/
+
+
 const entrada1 = window.prompt("Informe o primeiro número:")
 const entrada2 = window.prompt("Informe o segundo número:")
 
--- JavaScript/Exercício 2 - Teste de velocidade/index.js
+++ JavaScript/Exercício 2 - Teste de velocidade/index.js
@@ -3,3 +3,16 @@ Teste de Velocidade
 
 Escreva um programa em javascript que permita inserir o nome e a velocidade de dois veículos e exiba na tela uma mensagem dizendo qual dos dois é mais rápido (ou que as velocidades são iguais se este for o caso)
 */
+let veiculo1 = prompt("Informe o nome do primeiro veiculo:")
+let velocidade1 = parseFloat(prompt("Informe a velocidade do primeiro veiculo:"))
+
+let veiculo2 = prompt("Informe o nome do segundo veiculo:")
+let velocidade2 = parseFloat(prompt("Informe a velocidade do segundo veiculo:"))
+
+if(velocidade1 > velocidade2) {
+    alert(`${veiculo1} é mais rápido do que ${veiculo2}`)
+} else if(velocidade2 > velocidade1) {
+    alert(`${veiculo2} é mais rápido do que ${veiculo1}`)
+} else {
+    alert(`${veiculo1} e ${veiculo2} possuem velocidades iguais.`)
+}
